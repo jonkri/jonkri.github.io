dde92a5e5e82cf3506d77d0a3137ac790d1d78b5
Jon Kristensen <jon@nejla.com>
2021-01-26 13:27:52 +0100

    Add first version

---
+++ precache-manifest.a6d9674ea62d99f5982931f41e0a1360.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a36797ff1b291275197",
+    "url": "/css/app.8caa578c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d2f1ff81cce572ab46b1171acb92fb2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a36797ff1b291275197",
+    "url": "/js/app.9f1431f3.js"
+  },
+  {
+    "revision": "0d30d5fa5f76690633c8",
+    "url": "/js/chunk-vendors.919c39de.js"
+  },
+  {
+    "revision": "3c424340bd6d5c7e7a7b9b83fdd25be1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
